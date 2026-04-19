30c8686019f19ee9122095e2276ed282e888e6bc
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-04-18 23:25:51 -0300

    initial version of the terminal scroll added in kernel.c

--- kernel.c
+++ kernel.c
@@ -1,6 +1,6 @@
-#include<stdbool.h>
-#include<stddef.h>
-#include<stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 //list all the possible colors in vga mode
 enum vga_color {
@@ -24,12 +24,12 @@ enum vga_color {
 
 //function for setting the color in the terminal in the vga mode
 //returns a size_t with the value of the foreground and background
-uint16_t vga_entry_color(enum vga_color fg, enum vga_color bg)
+uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg)
 {
     return fg | (bg << 4);
 }
 
-uint16_t vga_entry(unsigned char c, uint16_t color)
+uint16_t vga_entry(unsigned char c, uint8_t color)
 {
     return (uint16_t) c | (uint16_t)(color << 8);
 }
@@ -40,28 +40,30 @@ uint16_t vga_entry(unsigned char c, uint16_t color)
 #define VGA_HEIGHT  25
 #define VGA_MEMORY  0xB8000 
 
-size_t terminal_row;
-size_t terminal_column;
+//global variables that determines the rows, columns, the terminal color and
+//a pointer to the adress to the vga video memory
+size_t terminal_row = 0;
+size_t terminal_column = 0;
 uint8_t terminal_color;
 volatile uint16_t* terminal_buffer = (volatile uint16_t*)VGA_MEMORY;
 
-
+//function to initialize the terminal with no characters
 void terminal_initialize(void)
 {
 
     terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-
+    uint16_t caractere = vga_entry(' ', terminal_color);
     for (size_t i = 0; i < VGA_HEIGHT; i++)
     {
         for (size_t j = 0; j< VGA_WIDTH; j++)
         {
-            uint16_t caractere = vga_entry(' ', terminal_color);
             size_t index = i * VGA_WIDTH + j;
             terminal_buffer[index] = caractere;
         }
     }
 }
 
+//function that returns the size of a string
 size_t strlen(const char* str)
 {
     size_t size = 0;
@@ -78,6 +80,8 @@ void terminal_setcolor(uint8_t color)
     terminal_color = color;
 }
 
+//function that writes in the screen and go to the next line
+//when find the '\n' character
 void vga_write(const char* info, size_t size)
 {
     for (size_t i = 0; i < size; i++)
@@ -90,7 +94,7 @@ void vga_write(const char* info, size_t size)
             if (terminal_row == VGA_HEIGHT)
                 terminal_row = 0;
 
-            break;
+            continue;
         }
 
         size_t index = terminal_row * VGA_WIDTH + terminal_column;
@@ -104,18 +108,29 @@ void vga_write(const char* info, size_t size)
 
         if (terminal_row == VGA_HEIGHT)
         {
-            terminal_row = 0;
+            size_t total = VGA_HEIGHT * VGA_WIDTH;
+            for(size_t i = 0; i < total; i++)
+            {
+                if (terminal_buffer[i] != ' ')
+                {
+                    terminal_buffer[i-VGA_WIDTH] = terminal_buffer[i];
+                }
+            }
         }
     }
 }
 
-void main(void)
+void kernel_main(void)
 {
-    char palavra[] = "test\n";
+    char word[] = "test\n";
     terminal_initialize();
     terminal_setcolor(vga_entry_color(VGA_COLOR_LIGHT_GREY,VGA_COLOR_BLACK));
-    vga_write(palavra,strlen(palavra));
+    vga_write(word,strlen(word));
     vga_write("hi\n",strlen("hi"));
+    for (int i = 0; i< 25; i++)
+    {
+        vga_write("\n",1);
+    }
 
     while(1);
 }
